d5586552405150ef978ef76b7c3c31c9dd17628a
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2022-11-14 19:21:47 +0300

    Add project files.

---
+++ Infrastructure/ETicaretAPI.Persistence/Conretes/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Conretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+        => new()
+        {
+            new(){ Id=Guid.NewGuid(), Name="Product 1", Stock=10, Price=50},
+            new(){ Id=Guid.NewGuid(), Name="Product 2", Stock=20, Price=60},
+            new(){ Id=Guid.NewGuid(), Name="Product 3", Stock=30, Price=70},
+            new(){ Id=Guid.NewGuid(), Name="Product 4", Stock=40, Price=80},
+            new(){ Id=Guid.NewGuid(), Name="Product 5", Stock=50, Price=90}
+        };
+    }
+}
