259800caea3a48a2670b24434195fafd1e3f98ad
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2022-11-15 19:11:32 +0300

    Veri eklenmeden öncki son hali, ServiceREgistration e ekleme öncesidir

---
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,48 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities.Common;
+using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Repositories
+{
+    public class ReadRepository<T> : IReadRepository<T> where T : BaseEntity
+
+    {
+        // Şuraya dikkat et 
+        private readonly ETicaretAPIDbContext _context;
+        public ReadRepository(ETicaretAPIDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+
+
+        public DbSet<T> Table => _context.Set<T>();
+
+        public IQueryable<T> GetAll()
+            => Table;
+
+
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method)
+        => Table.Where(method);
+
+
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method)
+            => await Table.FirstOrDefaultAsync(method);
+
+
+
+
+        public async Task<T> GetByIdAsync(string id)
+            => await Table.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
+        
+    }
+}
