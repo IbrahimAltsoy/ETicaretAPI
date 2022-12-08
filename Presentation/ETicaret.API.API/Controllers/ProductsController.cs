6359fba9d4e055148a59928eec0360c3eb7454bb
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2022-12-09 00:14:26 +0300

    9.12.2022

--- Presentation/ETicaret.API.API/Controllers/ProductsController.cs
+++ Presentation/ETicaret.API.API/Controllers/ProductsController.cs
@@ -73,7 +73,14 @@ namespace ETicaret.API.API.Controllers
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            return Ok(_productReadRepository.GetAll(false));
+            return Ok(_productReadRepository.GetAll(false).Select(p => new{
+                p.Id,
+                p.Name,
+                p.Stock,
+                p.Price,
+                p.CreatedDate,
+                p.UpdateTime
+            }));
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(string id)
@@ -84,7 +91,7 @@ namespace ETicaret.API.API.Controllers
         [HttpPost]
         public async Task<IActionResult> Post(VM_Create_Product model)
         {
-            if(ModelState.IsValid) { }
+            
             await _productWriteRepository.AddAsync(new()
             {
                 Name = model.Name,
